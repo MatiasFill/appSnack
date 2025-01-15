c90bf1262f28a8f6e62ce5659d57fb2ed9f796fb
MatiasFill <rmscontato@gmail.com>
2025-01-15 20:29:15 -0300

    Adicionei um arquivo .env e uma copia.env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,14 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3333'
   baseURL: 'http://192.168.1.102:3333'
- // baseURL: 'http://192.168.0.165:3333' // faixa de ip do Cicero
+  //baseURL: 'http://192.168.0.165:3333' // faixa de ip do Cicero
 })
+  */
 
-export { api };
+import axios from 'axios'
+export const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API || 'http://localhost:3333',
+    timeout: 10000,
+})
